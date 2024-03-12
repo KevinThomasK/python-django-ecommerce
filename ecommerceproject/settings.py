3761d7e9fe90810c4e116d68d4c1f83238b68c12
Kevin Thomas <kevinthomas0420@gmail.com>
2024-03-12 15:20:38 +0530

    files added for host

--- ecommerceproject/settings.py
+++ ecommerceproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-!qwq&na1*0%ebrei-dml+g*ou72jp26_pf$pst7+!94&q6tn$_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
